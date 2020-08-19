bc7c159807027b0638e418711525b4634761ae2a
nacho <nacho.mota@gmail.com>
2020-08-19 10:17:13 +0200

    Limpieza

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import React, { Component } from "react";
 import CardList from "./components/CardList";
 import Album from "./components/Album";
 
-//react bootstrap components
-// import CardDeck from "react-bootstrap/CardDeck";
-// import Card from "react-bootstrap/Card";
-// import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
-
-//data for post
-// import data from "./data.json";
 
 class App extends Component {
   constructor(props) {
@@ -22,11 +14,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https://images-api.nasa.gov/search?q=curiosity&page=5")
+    fetch("https://images-api.nasa.gov/search?q=curiosity&page=15")
       .then(response => response.json())
-      // .then(collection => {this.setState({ cards: collection})});
       .then(responseData => {
-        // console.log(responseData)
         this.setState({
           href: responseData.collection.href,
           items: responseData.collection.items,
@@ -37,7 +27,6 @@ class App extends Component {
 
   render() {
     const items = this.state.items;
-    // console.log(items)
     return (
       <Album>
         <CardList items={items} />
