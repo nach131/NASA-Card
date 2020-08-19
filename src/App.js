45d90b7fa32289a9243ead7e0a5a8e986b60e407
nacho <nacho.mota@gmail.com>
2020-08-19 13:29:18 +0200

    VACIO

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 //react bootstrap components
 // import CardDeck from "react-bootstrap/CardDeck";
-import Card from "react-bootstrap/Card";
+// import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 
@@ -10,7 +10,7 @@ import Row from "react-bootstrap/Row";
 // import style from "./styles.module.scss";
 
 //data for post
-import data from "./data.json";
+// import data from "./data.json";
 
 export default class DefaultPost extends Component {
 	render() {
@@ -19,27 +19,7 @@ export default class DefaultPost extends Component {
 				<Container fluid={true}>
 					<Row>
 
-						{data.map((postData, index) => (
-							<div className="col-xl-3 col-md-4">
-								<Card key={index} className="mb-4 shadow-sm">
-									<Card.Img variant="top" src={postData.links[0].href} />
-
-									{/* <h4>JSON: {postData.href}</h4> */}
-									<Card.Body>
-										<Card.Title className="mb-0">{postData.data[0].nasa_id}</Card.Title>
-										<div className="badges mb-2">
-
-											<span className="badge badge-warning">{postData.data[0].keywords + " "}</span>
-										</div>
-										{/* <p>TITULO:{postData.data[0].title}</p> */}
-										<Card.Text >{postData.data[0].description}</Card.Text>
-									</Card.Body>
-
-								</Card>
-
-							</div>
-
-						))}
+						<div>vacio</div>
 
 
 					</Row>
