d6599ae0f94b4cce8ffa4eee1546cd370238379a
nacho <nacho.mota@gmail.com>
2020-08-20 16:56:54 +0200

    Peticiona Api OK

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,45 +19,40 @@ class Header extends React.Component {
       diaHoy: date,
       loading: true,
       error: null,
+      fotosDias:[],
       fotodia: {},
     };
   }
 
   componentDidMount() {
     this.fetchFotoDay();
-
-    
   }
 
   masDoceDias() {
-  
     var dateFormat = require('dateformat');
     var current = new Date(); //Hoy
     var saltoDay = new Date(current.getTime() - 1036800000); // - 1 day in ms 86400000
     return dateFormat(saltoDay, "yyyy-mm-dd")
-
   }
-
 
 
   // componentDidUpdate(){
   //   this.fetchFotoDay();
   // }
   fetchFotoDay = async () => {
-    const Url = 
-      "https://api.nasa.gov/planetary/apod?api_key=EUDrgZhfxHUUr5GwtaCDh0FjiAxQsboz8BoR7MCY&date=";
-
-     
-      console.log(Url + this.masDoceDias());
-
+    const Url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=${this.masDoceDias()}&end_date=${this.state.diaHoy}`
+      // "https://api.nasa.gov/planetary/apod?api_key=EUDrgZhfxHUUr5GwtaCDh0FjiAxQsboz8BoR7MCY&date=";
+  
+    // console.log(Url + this.masDoceDias());
     this.setState({ loading: true, error: null });
     try {
-      const respuesta = await fetch(Url + this.state.diaHoy);
+      const respuesta = await fetch(Url);
       // const respuesta = await fetch(Url + this.masDoceDias());
       const respuestaData = await respuesta.json();
       console.log(respuestaData);
       this.setState({
-        fotodia: respuestaData,
+        // fotodia: respuestaData,
+        fotosDias: respuestaData,
         // diaHoy:this.state.diaHoy,
       });
     } catch (error) {
