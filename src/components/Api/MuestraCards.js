ac0534cc0dc6f88b91cd7480e3b331966b0a7428
nacho <nacho.mota@gmail.com>
2020-08-30 00:06:19 +0200

    Card API Buscador input

---
+++ src/components/Api/MuestraCards.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+
+class CardListItem extends React.Component {
+  render() {
+
+    let wordToRender;
+    if (this.props.postData.data[0].keywords) {
+      wordToRender = this.props.postData.data[0].keywords.map((word, i) => {
+        return (
+          <ul key={i} className="list-unstyled keyword">
+            <span className="badge badge-warning">
+              <li>{word}</li>
+            </span>
+          </ul>
+        );
+      });
+    }
+
+
+    return (
+      <>
+        <Card className="mb-4 shadow-sm">
+          {/* <Card.Img variant="top" src={this.props.item.links[0].href} /> */}
+          <Card.Body>
+            <Card.Title className="mb-0">
+            {this.props.postData.data[0].nasa_id}
+            </Card.Title>
+            <div className="badges mb-2">{wordToRender}</div>
+            <Card.Text>{this.props.postData.data[0].description}</Card.Text>
+          </Card.Body>
+        </Card>
+
+      </>
+    )
+  }
+}
+
+
+
+
+
+class MuestraGif extends Component {
+  // state = {  }
+  render() {
+    const items = this.props.MuestraItems
+    const loading = this.props.loading
+    return (
+      <Container fluid={true} className="py-4">
+        <Row>
+
+          {/* {items.map(item => <video autoPlay loop key={item} src={item} />)} */}
+          {loading ? (
+            <h1>DAME SI PAPI GIFS</h1>
+          ) : (
+              items.map((postData, i) => (
+                <div key="i" className="col-xl-3 col-md-4">
+                  {console.log(postData)}
+                  <CardListItem postData={postData} />
+                  {/* <p className="test">{postData.data[0].date_created}</p> */}
+                  {/* <p className="test">{postData.links[0].href}</p> */}
+                </div>
+
+              ))
+            )}
+        </Row>
+      </Container>
+
+    );
+  }
+}
+
+export default MuestraGif;
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Header from '../components/Header_data'
-import RenderApi from '../components/Test/RenderApi'
+// import RenderApi from '../components/Test/RenderApi'
+import BuscaApi from '../components/Api/BuscaApi'
+
 export default class Home extends Component {
 
   render() {
     return (
       <>
       <Header/>
-      <RenderApi/>
+      {/* <RenderApi/> */}
+      <BuscaApi/>
       </>
     )
   }
