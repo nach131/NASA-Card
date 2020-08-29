a54f6d884a199ca88a8b935ef09196eac284ae66
nacho <nacho.mota@gmail.com>
2020-08-29 21:23:10 +0200

    Gif separado en 3 componentes

--- src/components/Test/RenderApi.js
+++ src/components/Test/RenderApi.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { Navbar, Nav, Container, NavDropdown, Form, Button, InputGroup } from "react-bootstrap";
-
+import { Form, Button, InputGroup } from "react-bootstrap";
 
 import useGiphy from './useGiphy'
+import MuestraGif from './MuestraGif'
 
 export default function AsyncHooks() {
 
@@ -11,31 +11,9 @@ export default function AsyncHooks() {
   const [results, loading] = useGiphy(query);
   // console.log(results)
   return (
-    <div className="form px-5 ">
-      <h1>Componente separado</h1>
+    <div className="form px-5 pt-3">
+      {/* <h4>Busca por palabras</h4> */}
       <br />
-      {/* <form
-      className="align-self-center"
-        onSubmit={e => {
-          e.preventDefault();
-          setQuery(search);
-        }}
-      >
-        <div className="input-group">
-
-          <input
-            id="input" name="buscar"
-            className="form-control"
-            value={search}
-            onChange={e => setSearch(e.target.value)}
-            placeholder="Buscar Gifs!"
-          />
-          <div class="input-group-append">
-            <button className="btn btn-next" type="submit">Buscar</button>
-          </div>
-        </div>
-      </form> */}
-
       <Form className="align-self-center"
         onSubmit={e => {
           e.preventDefault();
@@ -58,11 +36,10 @@ export default function AsyncHooks() {
         </InputGroup>
       </Form>
       <br />
-      {loading ? (
-        <h1>GIVE ME GIFS</h1>
-      ) : (
-          results.map(item => <video autoPlay loop key={item} src={item} />)
-        )}
+ 
+      <MuestraGif MuestraTodosGifs={results}
+        loading={loading}
+      />
     </div>
   );
 }
