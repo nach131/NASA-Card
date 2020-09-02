a121b7432e6e22e9e24d5ae745a9c068e6a66239
nacho <nacho.mota@gmail.com>
2020-09-02 11:13:35 +0200

    Paginacion oculta

--- src/components/Api/BuscaApi.js
+++ src/components/Api/BuscaApi.js
@@ -4,6 +4,9 @@ import { Form, Button, InputGroup, Row, ButtonGroup } from "react-bootstrap";
 import useNasa from './useNasa'
 import MuestraCards from './MuestraCards'
 
+
+
+
 export default function AsyncHooks() {
 
   const [search, setSearch] = useState('');
@@ -13,6 +16,46 @@ export default function AsyncHooks() {
 
   const [results, loading] = useNasa(query, page);
   // console.log(results)
+
+
+  function MuestraPagina() {
+    if (query === '') {
+      return('')
+    }
+    if (page === 1) {
+      return (
+        <ButtonGroup aria-label="Basic example" className="mx-auto" size="sm">     
+          <Button className="btn-next" >{page}</Button>
+          <Button className="btn-next" onClick={e => {
+            e.preventDefault();
+            setQuery(tomate);
+            // setQuery('sun');
+            setPage(page + 1);
+          }} >Next</Button>
+        </ButtonGroup>
+      )
+    } else {
+      return (
+        <ButtonGroup aria-label="Basic example" className="mx-auto" size="sm">
+          <Button className="btn-next" onClick={e => {
+            e.preventDefault();
+            setQuery(tomate);
+            // setQuery('sun');
+            setPage(page - 1);
+          }}>Prev</Button>
+          <Button className="btn-next" >{page}</Button>
+          <Button className="btn-next" onClick={e => {
+            e.preventDefault();
+            setQuery(tomate);
+            // setQuery('sun');
+            setPage(page + 1);
+          }} >Next</Button>
+        </ButtonGroup>
+      )
+    }
+  }
+
+
   return (
     <div className="form pt-3">
       {/* <h4>Busca por palabras</h4> */}
@@ -40,35 +83,30 @@ export default function AsyncHooks() {
         </InputGroup>
       </Form>
       <br />
-
       <MuestraCards MuestraItems={results}
         loading={loading}
         className="col-xl-3 col-md-4"
       />
       <Row>
-      
-        <ButtonGroup aria-label="Basic example" className="mx-auto" size="sm">
-          <Button  className="btn-next" onClick={e => {
+      <MuestraPagina />
+        {/* <ButtonGroup aria-label="Basic example" className="mx-auto" size="sm">
+          <Button className="btn-next" onClick={e => {
             e.preventDefault();
             setQuery(tomate);
             // setQuery('sun');
             setPage(page - 1);
           }}>Prev</Button>
           <Button className="btn-next" >{page}</Button>
-          <Button  className="btn-next" onClick={e => {
+          <Button className="btn-next" onClick={e => {
             e.preventDefault();
             setQuery(tomate);
             // setQuery('sun');
             setPage(page + 1);
           }} >Next</Button>
-        </ButtonGroup>
-        {/* <Button className="btn-next mx-auto" onClick={e => {
-          e.preventDefault();
-          setQuery(tomate);
-          // setQuery('sun');
-          setPage(page + 1);
-        }}>Mas</Button> */}
+        </ButtonGroup> */}
+      
       </Row>
+    
 
     </div>
   );
